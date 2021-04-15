4d0c6921e084015c0cb8fcc9920012e919fb2cfb
eunjye <eunjh1220@gmail.com>
2021-04-15 17:17:07 +0900

    set sass

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import "@babel/polyfill"; // Promise, Object.assign, Array.from ...
 import { pi, power, Foo } from './lib';
 
 console.log(pi);
@@ -8,3 +9,11 @@ console.log(power(pi, pi));
 const f = new Foo();
 console.log(f.foo());
 console.log(f.bar());
+
+console.log(new Promise((resolve, reject) => {
+  setTimeout(() => resolve(1), 100);
+}));
+
+console.log(Object.assign({}, { x: 1 }, { y: 2}));
+
+console.log(Array.from([1, 2, 3], v => v + v));
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   // entry file
-  entry: './src/js/main.js',
+  entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
   // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
   output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
   },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'css/style.css' })
+  ],
   module: {
     rules: [
       {
@@ -15,14 +19,23 @@ module.exports = {
         include: [
           path.resolve(__dirname, 'src/js')
         ],
-        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
             plugins: ['@babel/plugin-proposal-class-properties']
           }
-        }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader", // translates CSS intro CommonJS
+          "sass-loader" // compiles SASS to CSS, using Node SASS by default
+        ],
+        exclude: /node_modules/
       }
     ]
   },
